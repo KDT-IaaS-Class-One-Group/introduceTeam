0c46f524ddd993bc80abb299d8fee18c6c10dca6
HongMoonGI <hmoongi94@gmail.com>
2023-11-30 17:33:40 +0900

    why: 클릭시 addeventlistener를 쓰는거랑 해쉬태그랑의 연관이 있는지 궁금함

--- public/static/js-module/menuComponent.js
+++ public/static/js-module/menuComponent.js
@@ -8,7 +8,7 @@ export function menuComponent(stateData, onMenuClick) {
   const menuItems = [];
   for (let i = 0; i < stateData.length; i++) {
     const item = stateData[i]
-    const menuItem = createElement('li', { onClick: () => onMenuClick(item.hash)}, createElement('a', { href: item.hash }, item.text))
+    const menuItem = createElement('li', {}, createElement('a', { href: item.hash, onClick: () => onMenuClick(item.hash) }, item.text))
     //* 콜백함수를 사용해서 hash이름일 때 onclick함수를 실행한다.
     menuItems.push(menuItem)
   }
@@ -16,5 +16,5 @@ export function menuComponent(stateData, onMenuClick) {
   const menu = createElement('ul', {class: 'menu'}, ...menuItems)
   // const content = createElement('div', {}, 'Hello React')
 
-  return createElement('div', {}, menu, content)
+  return createElement('div', {}, menu)
 }
