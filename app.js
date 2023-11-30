53e646714ce9de80161496d4f9d57674a6026a80
kimhyun <forhn37@gmail.com>
2023-11-30 15:36:35 +0900

    feat: 김현 기본 페이지 제작

---
+++ app.js
@@ -0,0 +1,55 @@
+function createElement(type, props, ...children) {
+  return {type, props, children}
+};
+// 
+const stateData = [
+  { hash: '#home', text: 'Home' },
+  { hash: '#about', text: 'About' },
+  { hash: '#services', text: 'Services' },
+  { hash: '#portfolio', text: 'portfolio' },
+  { hash: '#contact', text: 'Contact' }
+];
+function component(stateData) {
+  const menuItems = [];
+  for (let i = 0; i < stateData.length; i++) {
+    const item = stateData[i]
+    const menuItem = createElement('li', {}, createElement('a', { href: item.hash }, item.text))
+    // return값은 {type : 'li', props: {}, children : [type: 'a', props : {href:item.hash}, children : item.text] }
+
+
+    menuItems.push(menuItem)
+
+  }
+  const menu = createElement('ul', {}, ...menuItems)
+
+  const content = createElement('div', {}, 'Hello React')
+
+  return createElement('div', {}, menu, content)
+
+}
+function render(virtualDom) {
+  
+  
+  if (typeof virtualDom === 'string') {
+    return document.createTextNode(virtualDom)
+    // return textcontent = virutualdom
+  }
+  // te
+  const element = document.createElement(virtualDom.type)
+  if (virtualDom.props) {
+    for (const [key, value] of Object.entries(virtualDom.props)) {
+      element.setAttribute(key, value)
+    }
+  }
+  // <div key ="value"> 
+  for (let i = 0; i < virtualDom.children.length; i++) {
+    const child = virtualDom.children[i]
+    element.appendChild(render(child))
+  }
+
+  return element
+}
+
+const virtualDom = component(stateData)
+const container = document.getElementById('root')
+container.appendChild(render(virtualDom))
