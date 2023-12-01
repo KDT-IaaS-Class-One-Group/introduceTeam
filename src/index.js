c5d2b17f89fe3006d11594fc0d454f49edd7c8e3
이민구 <escapeweedy@gmail.com>
2023-12-01 10:15:17 +0900

    feat: hash 변경 시 컴포넌트 로드

--- src/index.js
+++ src/index.js
@@ -12,31 +12,3 @@ render(header(), 'root');
 render(menu(teamData), 'root');
 render(section(), 'root');
 render(footer(), 'root');
-
-window.addEventListener("hashchange", () => {
-  const intro = document.getElementById("intro");
-
-  const hash = window.location.hash.substr(1);
-
-  switch (hash) {
-    case "김현":
-      intro.innerHTML = "a";
-      break;
-    case "신동현":
-      intro.innerHTML = createElement('p', { style: "color:blue;" }, ["gd"]);
-      break;
-    case "윤준현":
-      intro.innerHTML = createElement('p', { style: "color:blue;" }, ["gd"]);
-      break;
-    case "이민구":
-      intro.innerHTML = createElement('p', { style: "color:blue;" }, ["gd"]);
-      break;
-    case "홍문기":
-      intro.innerHTML = createElement('p', { style: "color:blue;" }, ["gd"]);
-      break;
-    default:
-      intro.innerHTML = createElement("h1", {}, ["반갑습니다. 접속할 때 보이는 페이지(처럼보이는) element입니다. "])
-  }
-
-  window.dispatchEvent(new Event("hashchange"));
-});
