e39f37ed9e0be97ef972ed99ae2c93eae070dbf5
HongMoonGI <hmoongi94@gmail.com>
2023-12-01 10:54:28 +0900

    feat: 이미 클릭해서 만들었던 component를 삭제 후에 다시 생성하도록 제작

--- public/static/js-module/onMenuClick.js
+++ public/static/js-module/onMenuClick.js
@@ -6,14 +6,15 @@ import { render } from "./render.js";
 import { introduceData } from "../data/introduce.js"
 
 export function onMenuClick(hash) {
+  const container = document.getElementById('root')
   // 각 메뉴 항목을 클릭할 때 호출되는 함수
   // hash에 따라 내용을 동적으로 변경할 수 있습니다.
   // 여기에서는 단순히 콘솔에 로그를 출력하는 예시를 작성했습니다.
+  if(container.children.length >=2 ){
+    container.removeChild(container.children[1])
+  }
+  
   let withoutHashtag = hash.slice(1);
-  // console.log(`Menu item clicked: ${withoutHashtag}`);
-  // console.log(typeof(withoutHashtag))
-
-  const container = document.getElementById('root')
   const introducePersonData = introduceData[withoutHashtag]
   // console.log(introducePersonData)
 
