da373c65b450befa034b0a99394595a069cc4601
이민구 <escapeweedy@gmail.com>
2023-12-04 11:53:03 +0900

    팀 소개 페이지 새로 시작

--- src/components/footer.js
+++
@@ -1,5 +0,0 @@
-import createElement from '../utils/createElement';
-
-export function footer() {
-  return createElement('footer', {}, createElement('p', {}, '\u00A9 2023 form 미쳤다'));
-}
--- src/components/menu.js
+++
@@ -1,9 +0,0 @@
-import createElement from '../utils/createElement';
-
-export function menu(stateData) {
-  const menuItems = stateData.map(item =>
-    createElement('li', {}, createElement('a', { href: item.hash }, item.name))
-  );
-
-  return createElement('nav', {}, createElement('ul', {}, ...menuItems));
-}
