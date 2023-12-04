d73a9be4a4253846d677dc1a89152ca2724455cb
HongMoonGI <hmoongi94@gmail.com>
2023-12-04 15:29:13 +0900

    fix: YoonJieut/issue14 버그수정

--- selectDivs.js
+++ selectDivs.js
@@ -6,7 +6,6 @@ export default function selectDivs(callback) {
         divs.forEach((x)=>{x.style.visibility = 'hidden';})
         callback(); // 콜백 함수 실행
         event.stopPropagation();
-        
       });
     });
 }
