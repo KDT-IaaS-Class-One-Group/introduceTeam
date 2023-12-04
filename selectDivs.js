99319bb8d0e658b494a726bf886729d1e65d6c0b
HongMoonGI <hmoongi94@gmail.com>
2023-12-04 15:31:36 +0900

    feat: 주석정리

--- selectDivs.js
+++ selectDivs.js
@@ -4,7 +4,7 @@ export default function selectDivs(callback) {
   divs.forEach(div => {
       div.addEventListener('click', () => {
         divs.forEach((x)=>{x.style.visibility = 'hidden';})
-        callback(); // 콜백 함수 실행
+        callback(); // 콜백함수: 템플릿 페이지 불러오는 함수 넣을자리
         event.stopPropagation();
       });
     });
