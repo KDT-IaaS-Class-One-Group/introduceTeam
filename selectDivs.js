3368a2430e59617cdc99c0ebd80bdf51c1f9775a
YoonJunHyeon <qmdldos@gmail.com>
2023-12-04 14:24:42 +0900

    bug : 모듈화 진행 중 오류 발생

---
+++ selectDivs.js
@@ -0,0 +1,10 @@
+// 1번 모듈: Div 선택 로직
+export default function selectDivs(callback) {
+  const divs = document.querySelectorAll('div');
+  divs.forEach(div => {
+      div.addEventListener('click', () => {
+        divs.forEach((x)=>{x.style.visibility = 'hidden';})
+        callback(); // 콜백 함수 실행
+      });
+  });
+}
