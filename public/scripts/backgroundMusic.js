422d64316f186945dfc4b5894ffd27e0f8523913
이민구 <escapeweedy@gmail.com>
2023-12-05 16:17:57 +0900

    feat: 디자인 예시안

--- public/scripts/backgroundMusic.js
+++ public/scripts/backgroundMusic.js
@@ -1,3 +1,10 @@
-export default document.addEventListener('mousemove', () => {
-  document.getElementById('bgm').play();
-});
+export default function backgroundMusic() {
+  const audioElement = document.createElement('audio');
+  const sourceElement = document.createElement('source');
+  audioElement.id = 'bgm';
+  audioElement.autoplay = true;
+  sourceElement.src = './sounds/bgm.mp3';
+  sourceElement.type = 'audio/mp3';
+  audioElement.appendChild(sourceElement);
+  document.getElementById('root').appendChild(audioElement);
+}
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,5 +1,21 @@
 import animateTeamMemberName from './animateTeamMemberName.js';
 import backgroundMusic from './backgroundMusic.js';
+// import teamMemberData from '../data/teamMemberData.js'
 
 animateTeamMemberName();
 backgroundMusic();
+
+  const teamMemberNameDivs = document.querySelectorAll(".teamMemberName > div");
+  const teamMemberIntroduceDivs = document.querySelectorAll(".teamMemberIntroduce");
+
+  teamMemberNameDivs.forEach((nameDiv, index) => {
+    nameDiv.addEventListener("click", () => {
+      teamMemberIntroduceDivs.forEach((introDiv, introIndex) => {
+        if (index === introIndex) {
+          introDiv.classList.toggle("active");
+        } else {
+          introDiv.classList.remove("active");
+        }
+      });
+    });
+  });
