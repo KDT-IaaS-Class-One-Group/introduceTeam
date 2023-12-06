25a637612b8cb503049fca47b41f0a16278a3912
HongMoonGI <hmoongi94@gmail.com>
2023-12-06 17:51:32 +0900

    feat: 줄편집

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,12 +3,10 @@ import backgroundMusic from './backgroundMusic.js';
 // import teamMemberData from '../data/teamMemberData.js'
 import animateTeamIntroduce from './animate/animateTeamIntroduce.js';
 import showIntroduce from './showIntroduce.js'
-
 import clickSoundTeamMemberName from './clickSound/clickSoundTeamMemberName.js'
 
 showIntroduce();
 animateTeamMemberName();
 backgroundMusic();
 animateTeamIntroduce();
-
 clickSoundTeamMemberName();
