03b6b2c01c00d3ae7beb8f0243b315679db660d8
동현 <Garry@example.com>
2023-12-06 17:24:33 +0900

    feat : index.js에 MemberClickSound 모듈 추가

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,6 +3,10 @@ import backgroundMusic from './backgroundMusic.js';
 // import teamMemberData from '../data/teamMemberData.js'
 import animateTeamIntroduce from './animate/animateTeamIntroduce.js';
 
+import clickSoundTeamMemberName from './clickSound/clickSoundTeamMemberName.js'
+
 animateTeamMemberName();
 backgroundMusic();
 animateTeamIntroduce();
+
+clickSoundTeamMemberName();
