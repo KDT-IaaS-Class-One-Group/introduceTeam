0b16674b7f565d59d6d5de18c39bcad5000da958
kimhyun <forhn37@gmail.com>
2023-12-07 14:06:36 +0900

    feat : 모듈 설정 중

---
+++ test/randomFontColor.js
@@ -0,0 +1,11 @@
+import { randomColorname } from "./test"
+
+function inrandomcolor() {
+  const teamMemberName = document.querySelectorAll('.teamMemberName > div')
+  
+  teamMemberName.forEach((element) = {
+    element.addEventListener
+  })
+}
+
+
