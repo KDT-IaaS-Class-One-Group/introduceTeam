e6ec752bf3d68622991e4db9d132cd46ae481e96
YoonJunHyeon <qmdldos@gmail.com>
2023-12-07 20:11:02 +0900

    메모 작성 후 마무리

--- public/scripts/animate/animateTeamIntroduce.js
+++ public/scripts/animate/animateTeamIntroduce.js
@@ -2,17 +2,24 @@ export default function animateTeamIntroduce() {
   // * 변수 이름이 적절한 것 같지 않으나 통일감을 위해서 그대로 작성한다.
   const teamMemberName = document.querySelectorAll('.teamMemberName > div');
   const introduceElement = document.querySelector('.teamMemberIntroduce');
+  const profileName = document.querySelector('.profile > h3');
+
+  // memo1 : style.left가 -100 일때, 클릭한 것이 물고기 이름과 프로필 이름이 다르면 들어갔다가 상태를 바꾸고 다시 창을 띄워줍니다.
+  // memo2 : 시작은 클릭한 것을 비교하여 그대로 가져온다.
+  // memo3 : 여기서 나온 메모들을 모으는 실행 함수를 만들어야 한다.
 
   teamMemberName.forEach((div) => {
     div.addEventListener('click', function () {
-      // 선택이 이미 되어있는지 체크
-      if (introduceElement.style.left !== '-100%') {
-        introduceElement.style.left = '-100%'
-        setTimeout(() => {
-          introduceElement.style.left = '0%';
-        }, 500); // 0.5초
-      } else {
-        introduceElement.style.left = '-100%';
+      console.dir(div);
+      console.log(profileName.innerText);
+      // 클릭된 이름과 프로필이름이 동일하지 않으면
+      if(div.innerHTML !== profileName.innerText){
+        // 올라간다.
+        if (introduceElement.style.left == '-100%') {
+          introduceElement.style.left = '0';
+        } else {
+          introduceElement.style.left = '-100%';
+        }
       }
     });
   });
--- public/scripts/fontcolor/randomFontColor.js
+++ public/scripts/fontcolor/randomFontColor.js
@@ -3,7 +3,6 @@ import randomColorname from "./randomColorname.js"
 export default function inrandomcolor(time) {
   const teamMemberName = document.querySelectorAll('.teamMemberName > div')
 
-  console.log(teamMemberName);
 // setInterval을 통하여 각 요소에 대하여 램덤한 색상을 넣는 함수로 수정
   setInterval(() => {
     teamMemberName.forEach(element => {
