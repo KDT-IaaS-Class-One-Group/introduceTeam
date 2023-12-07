9757d5c76fd957d9daefdba5e7e52b646cbc8411
kimhyun <forhn37@gmail.com>
2023-12-07 15:58:44 +0900

    feat : 오류 해결 - 파일확장자 문제

--- test/random.js
+++ test/random.js
@@ -1,7 +1,7 @@
 // 랜덤한 숫자를 만드는 함수
 // RGB는 16진수로 표현되므로 (0~f까지 표시) 랜덤함수의 최대값은 16으로 작성
 // to.String(16)으로 처리시 16진수로 바뀜
-export default function randomColorname () {
+export default function randomColorname() {
 
   function getRandomDigit() {
     const randomdigit = Math.floor(Math.random() * 16)
@@ -22,4 +22,4 @@ export default function randomColorname () {
 }
 
 
-console.log(randomColorname())
+// console.log(randomColorname())
--- test/randomFontColor.js
+++ test/randomFontColor.js
@@ -1,9 +1,9 @@
-import randomColorname from "./test"
+import randomColorname from "./random.js"
 
 export default function inrandomcolor() {
   const teamMemberName = document.querySelectorAll('.teamMemberName > div')
   
-  console.log(randomColorname())
+  console.log('hi')
   // teamMemberName.forEach((element) => {
   //   element.addEventListener('click', function() {
   //     console.log(element)
