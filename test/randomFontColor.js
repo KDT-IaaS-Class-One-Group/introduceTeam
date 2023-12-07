ed8bfdffe9726e54a118bf0f92e228c02135a27d
kimhyun <forhn37@gmail.com>
2023-12-07 16:43:06 +0900

    feat : setInterval 함수를 통한 변경 요청

--- test/randomFontColor.js
+++ test/randomFontColor.js
@@ -2,13 +2,12 @@ import randomColorname from "./random.js"
 
 export default function inrandomcolor() {
   const teamMemberName = document.querySelectorAll('.teamMemberName > div')
-  
-  console.log(teamMemberName)
-  teamMemberName.forEach((element) => {
-    element.addEventListener('click', function(event) {
-      event.target.style.color = randomColorname()
-      // event. = `RGB(${randomColorname})`
-    })
-  })
-}
 
+  console.log(teamMemberName);
+
+  setInterval(() => {
+    teamMemberName.forEach(element => {
+      element.style.color = randomColorname()
+    })
+  }, 500);
+}
