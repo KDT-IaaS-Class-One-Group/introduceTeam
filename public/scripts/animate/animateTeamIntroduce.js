a38bfc882081ecac8708f308158c851cd3f99a9c
동현 <Garry@example.com>
2023-12-07 18:05:20 +0900

    feat: member div 클릭 후 애니메이션 효과 수정 특이사항 : 0.5초후에 해당 애니메이션이 작동하도록 클릭 이벤트 내 setTimeout를 사용하여 딜레이 추가

--- public/scripts/animate/animateTeamIntroduce.js
+++ public/scripts/animate/animateTeamIntroduce.js
@@ -4,13 +4,16 @@ export default function animateTeamIntroduce() {
   const introduceElement = document.querySelector('.teamMemberIntroduce');
 
   teamMemberName.forEach((div) => {
-    div.addEventListener('click', function() {
-      // Toggle the left position of .teamMemberIntroduce
-      if (introduceElement.style.left === '-100%') {
-        introduceElement.style.left = '0%';
+    div.addEventListener('click', function () {
+      // 선택이 이미 되어있는지 체크
+      if (introduceElement.style.left !== '-100%') {
+        introduceElement.style.left = '-100%'
+        setTimeout(() => {
+          introduceElement.style.left = '0%';
+        }, 500); // 0.5초
       } else {
         introduceElement.style.left = '-100%';
       }
     });
-  })
+  });
 }
