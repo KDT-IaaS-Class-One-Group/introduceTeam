960222c4b32c6037d7d21304c4dab59e2d2b3de3
kimhyun <forhn37@gmail.com>
2023-12-08 11:11:59 +0900

    feat : bgm문제 해결 오디오 요소를 여러개 만드는 것에서 문제가 발생하였음 오디오 요소가 없는 조건식 추가

--- public/scripts/backgroundMusic.js
+++ public/scripts/backgroundMusic.js
@@ -1,10 +1,15 @@
+// backgroundMusic.js
+let audioElement; // 단일 인스턴스를 저장할 변수
+
 export default function backgroundMusic() {
-  const audioElement = document.createElement('audio');
-  const sourceElement = document.createElement('source');
-  audioElement.id = 'bgm';
-  audioElement.autoplay = true;
-  sourceElement.src = './sounds/bgm.mp3';
-  sourceElement.type = 'audio/mp3';
-  audioElement.appendChild(sourceElement);
-  document.getElementById('root').appendChild(audioElement);
+  if (!audioElement) {
+    audioElement = document.createElement('audio');
+    const sourceElement = document.createElement('source');
+    audioElement.id = 'bgm';
+    audioElement.autoplay = true;
+    sourceElement.src = './sounds/bgm.mp3';
+    sourceElement.type = 'audio/mp3';
+    audioElement.appendChild(sourceElement);
+    document.getElementById('root').appendChild(audioElement);
+  }
 }
