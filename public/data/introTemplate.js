ebd6e08e47e4fcf09d4b5f3f224ad336ea509f65
YoonJunHyeon <qmdldos@gmail.com>
2023-12-08 11:00:57 +0900

    fix : data 수정 및 추후에 사용하면 좋을 템플릿 함수 작성

--- public/data/introTemplate.js
+++ public/data/introTemplate.js
@@ -1,17 +1,20 @@
-// 팀원고 논의하지 않은 모듈 작성
+// 추후 사용하면 좋을 것 같은 로직
+// 팀원과 논의하지 않은 모듈 작성
+
 
 /**
  * 각 대입에 맞춰서 data 전용, html 템플릿을 리턴합니다.
  * 
- * @param {string} imageSrc 
- * @param {string} githubUrl 
- * @param {string} introduceYourSelf : html작성 방식처럼 작성하여도 좋습니다.
+ * @param {string} teamMember : 맴버 자신 이름이 들어갑니다.
+ * @param {string} imageUrl : git 아바타 
+ * @param {string} githubUrl : git 주소
+ * @param {string} introduceYourSelf : p태그 안에 작성됩니다.html작성 방식처럼 작성하여도 좋습니다.
  * @returns html 템플릿이 작성됩니다.
  */
-export default function introTemplate(imageSrc,githubUrl,introduceYourSelf ){
+export default function introTemplate(teamMember,imageUrl,githubUrl,introduceYourSelf ){
   return '`'+`<div class="profile">
-    <img src="${imageSrc}" alt="프로필 사진"> 
-    <h3>김현</h3>
+    <img src="${imageUrl}" alt="프로필 사진"> 
+    <h3>${teamMember}</h3>
     <p>GitHub : <a href="${githubUrl}">${githubUrl}</a>
     </p>
   </div>
